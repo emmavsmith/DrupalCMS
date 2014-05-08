9167fef392f349afc57ecce513a3385a37aef0b6
emmavsmith <emma.smith@mmu.ac.uk>
2014-05-08 16:35:10 +0100

    Added DetailWebView class

---
+++ iOS/DrupalCMS/DrupalCMS/Views/DetailWebView.h
@@ -0,0 +1,13 @@
+//
+//  DetailWebView.h
+//  DrupalCMS
+//
+//  Created by Emma Smith on 08/05/2014.
+//  Copyright (c) 2014 mmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailWebView : UIWebView
+
+@end
